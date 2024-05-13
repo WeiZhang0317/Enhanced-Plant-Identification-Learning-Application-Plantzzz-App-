6cfb96c6512fd7496f2ff3b6255766d9b9b2ea10
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2024-05-13 17:53:17 +1200

    add animation 1

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../images/logo.png";
+import leaf from "../images/leaf.gif"; // Import the leaf.gif
 import { useNavigate } from "react-router-dom";
 import '../styles/HomePage.css'; // Importing the CSS file with specific selectors
 
@@ -16,7 +17,10 @@ function HomePage() {
 
   return (
     <div className="homepage">
-      <img src={logo} alt="Plantzzz Logo" className="homepage-logo"/>
+      <div className="logo-container"> {/* Container for logos */}
+        <img src={logo} alt="Plantzzz Logo" className="homepage-logo"/>
+        <img src={leaf} alt="Leaf Animation" className="leaf-gif"/> {/* New Leaf GIF */}
+      </div>
       <button
         onMouseOver={(e) => e.currentTarget.classList.add('homepage-button-hovered')}
         onMouseOut={(e) => e.currentTarget.classList.remove('homepage-button-hovered')}
