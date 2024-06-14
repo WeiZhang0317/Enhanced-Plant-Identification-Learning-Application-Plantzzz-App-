68aa9f18049c3b953cd12c90fa60e698bc7d53de
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2024-06-14 17:25:08 +1200

    loader update

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,8 +4,8 @@ import '../styles/Loader.css'; // Import the CSS file for the loader
 function Loader() {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
-      <div className="loader-text">Loading...</div> {/* Add loading text */}
+      {/* <div className="loader"></div> */}
+      <div class="load_16">Loading...</div>
     </div>
   );
 }
